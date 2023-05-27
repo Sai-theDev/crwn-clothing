cbf073f78a6d043a541ec14b0a4a86d53a97cfd2
MaxLyon001 <68329001+MaxLyon001@users.noreply.github.com>
2023-05-27 10:05:32 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes,Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component"
+
+import Home from "./routes/home/home.component"
+
+const Shop=()=>{
+  return (<h1>I am Shop</h1>)
+}
+
+const App=()=>{
+    return (
+      <Routes> 
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />}/>
+          <Route path="shop" element={<Shop />} />
+        </Route>
+      </Routes>
+    );
+}
+
+export default App;
