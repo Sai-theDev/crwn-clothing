669527b07024ecb0e2cb9f8627fb851d2820a48d
MaxLyon001 <68329001+MaxLyon001@users.noreply.github.com>
2023-05-30 18:20:10 +0530

    Adding sign in component with firebase

--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -37,8 +37,27 @@ export const signInWithGooglePopup = () =>signInWithPopup(auth,provider);
 export const db=getFirestore();
 
 
-export const createUserDocumentFromAuth = async(userAuth)=>{
-  const userDocRef = doc(db,'users', userAuth.uid);
-  console.log(userDocRef)
-}
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
 
