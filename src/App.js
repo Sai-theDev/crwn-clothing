b01f93e9ff4790ca8aaba18c72c033866236ad2a
MaxLyon001 <68329001+MaxLyon001@users.noreply.github.com>
2023-06-22 10:00:28 +0530

    Adding Functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { Routes,Route } from "react-router-dom";
-import Navigation from "./routes/navigation/navigation.component"
+import { Routes, Route } from 'react-router-dom';
 
-import Home from "./routes/home/home.component"
-import Signin from "./routes/sign-in/sign-in.component"
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
-const Shop=()=>{
-  return (<h1>I am Shop</h1>)
-}
-
-const App=()=>{
-    return (
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path="shop" element={<Shop />} />
-          <Route path="sign-in" element={<Signin />} />
-        </Route>
-      </Routes>
-    );
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-
 import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 
+import './index.scss';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById('root');
+
+render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />     
-    </BrowserRouter>  
+      <UserProvider>
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
+      </UserProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  rootElement
 );
-
-
